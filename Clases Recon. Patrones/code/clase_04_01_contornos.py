60d8fbe742df69f01b2a681206eb6998fe0058cd
Edu Carrasco <e.carrascovidal@hotmail.com>
2022-07-19 20:00:38 -0400

    MAR192000 JUL 2022

---
+++ Clases Recon. Patrones/code/clase_04_01_contornos.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+#imagen inventada
+bw=[[0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]]
+
+#transformamos los datos a uint8
+binary = np.array(bw, dtype='uint8')
+
+#buscamos las coordenadas de los contornos 
+contours, hierarchy = cv2.findContours(binary, 
+                                        cv2.RETR_EXTERNAL, 
+                                        cv2.CHAIN_APPROX_NONE)
+
+#generamos una matriz (fila/columna) con los contornos
+contornos = np.vstack(contours[0])
+
+#graficamos
+plt.figure(dpi=150)
+plt.imshow(binary, cmap='gray')
+plt.scatter(contornos[:,0], contornos[:,1], color='red', marker='x')
+plt.show()
--- Curso de Reconocimiento de Patrones en Imagenes-20220718/code/clase_02_01_lectura.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-
-#lectura de imagen
-img = cv2.imread('lena.jpg')
-
-plt.imshow(img)
-plt.show()
